291bcc9f10a43fb5236aa9046c5c331d294e6137
drpt2c <drpt2c@mst.edu>
2017-04-26 11:48:34 -0500

    I think I uploaded the wrong file
    This one does work

--- FileToGraph.py
+++ FileToGraph.py
@@ -7,46 +7,51 @@ Stat_X = [] #static time
 Stat_Y = [] #static data
 Reac_X = [] #reactive time
 Reac_Y = [] #reactive data
-Rand_X = [] #random time
-Rand_Y = [] #random data
+#Rand_X = [] #random time
+#Rand_Y = [] #random data
 
 #variables
 ran = False #this is for reading word by word
-
+done = False
+Redone = True
 with open("data.dat", "r") as f: #read data in from the file
     for line in f:
-        if line is "Static": #this adds the data for the static attack
-            checker = False
+        if "Static" in line or done is not True: #this adds the data for the static attack
+            ran = False
             for word in line.split():
-                if ran is not True:
-                    Stat_X = word
-                    ran = True
-                else:
-                    Stat_Y = word
-        elif line is "Reactive": #this adds the data for the reactive attack
-            checker = False
+                if "End" in line:
+                    done = True
+                    Redone = False
+                if "Reactive" not in line and "Static" not in line and done is not True:
+                    if ran is not True and "Static" not in line:
+                        Stat_X.append(word)
+                        ran = True
+                    else:
+                        Stat_Y.append(word)
+            else:
+                continue
+        elif "Reactive" in line or Redone is not True or done is True:
+            ran = False
             for word in line.split():
-                if ran is not True:
-                    Reac_X = word
-                    ran = True
-                else:
-                    Reac_Y = word
-        else: #this adds the data for random attack
-            checker = False
-            for word in line.split():
-                if ran is not True:
-                    Rand_X = word
-                    ran = True
-                else:
-                    Rand_Y = word
+                if "End" in line:
+                    Redone = True
+                if "Reactive" not in line and Redone is not True:
+                    if ran is not True and "Reactive" not in line:
+                        Reac_X.append(word)
+                        ran = True
+                    else:
+                        Reac_Y.append(word)
 
+        else:
+            print("Failed to match")
 
+check = True
 
 
 plt.suptitle("Flow Over Time", fontsize=30)
 plt.plot(Stat_X, Stat_Y, "r-", label="Stat")
 plt.plot(Reac_X, Reac_Y, "b-", label="Reac")
-plt.plot(Rand_X, Rand_Y, "y-", label="Rand")
+#plt.plot(Rand_X, Rand_Y, "y-", label="Rand")
 legend(bbox_to_anchor=(1.05, 1), loc=1, borderaxespad=0)
 plt.ylabel("Data")
 plt.xlabel("Time Round")
